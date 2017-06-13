c72a93dbdcceff31bb5e0be1aa46df1383abf23e
TheScottishFly <TheScottishFly@users.noreply.github.com>
2017-06-13 18:53:02 +0200

    init position class and add str method at all

--- model.py
+++ model.py
@@ -6,18 +6,35 @@ from sys import argv
 
 
 class Agent:
+    """Classe qui représente une personne"""
 
     def __init__(self, attributes_from_dict):
         for key, value in attributes_from_dict.items():
             self.__setattr__(key, value)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class Position:
+    """Classe qui représente une zone"""
+
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return str(self.__dict__)
 
 
 def populate(filename):
     with open(filename, 'r') as json_file:
         datas = load(json_file)
 
+        position = Position(datas[0].pop('latitude'), datas[0].pop('longitude'))
         first_agent = Agent(datas[0])
-        print(first_agent.conscientiousness)
+        print(position)
+        print(first_agent)
 
 if __name__ == '__main__':
     populate(argv[1])
