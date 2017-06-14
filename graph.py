3a92c9709b06c9994edce0d2da1021c861e97ab4
TheScottishFly <TheScottishFly@users.noreply.github.com>
2017-06-14 17:35:06 +0200

    init agreeablenessgraph

--- graph.py
+++ graph.py
@@ -24,3 +24,17 @@ class BaseGraph(ABC):
         plt.title(self.title)
         plt.grid(self.show_grid)
         plt.show()
+
+
+class AgreeablenessGraph(BaseGraph):
+
+    def __init__(self):
+        super().__init__()
+        self.title = "Nice people live in the countryside"
+        self.x_label = "population density"
+        self.y_label = "agreeableness"
+
+    def xy_values(self, zones):
+        x_values = [zone.population_density() for zone in zones]
+        y_values = [zone.average_agreeableness() for zone in zones]
+        return x_values, y_values
